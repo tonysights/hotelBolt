08036a73234de03e7a781ec4bf36a2a76cf4cc87
TonySight <tonysight@outlook.com>
2020-04-24 13:51:27 +0800

    修正splash特效透明度和颜色

--- app/src/main/java/xyz/tspace/hotelbolt/view/launch/SplashFragment.kt
+++ app/src/main/java/xyz/tspace/hotelbolt/view/launch/SplashFragment.kt
@@ -58,16 +58,14 @@ class SplashFragment :
 
     }
 
-    private fun initPoint() {
 
+    private fun initPoint() {
         val height: Int = QMUIDisplayHelper.getScreenHeight(requireContext())
         val width: Int = QMUIDisplayHelper.getScreenWidth(requireContext())
-
         val centerX = width / 2
         val centerY = height / 2
 
-
-        val circleBean = CircleBean(
+        val cb1 = CircleBean(
             PointF((-width / 5.1).toFloat(), (height / 1.5).toFloat()),
             PointF((centerX - 30).toFloat(), (height * 2 / 3).toFloat()),
             PointF((width / 2.4).toFloat(), (height / 3.4).toFloat()),
@@ -75,7 +73,8 @@ class SplashFragment :
             PointF((width / 7.2).toFloat(), (-height / 128).toFloat()),
             (width / 14.4).toFloat(), 60
         )
-        val circleBean2 = CircleBean(
+
+        val cb2 = CircleBean(
             PointF((-width / 4).toFloat(), (height / 1.3).toFloat()),
             PointF((centerX - 20).toFloat(), (height * 3 / 5).toFloat()),
             PointF((width / 2.1).toFloat(), (height / 2.5).toFloat()),
@@ -83,7 +82,7 @@ class SplashFragment :
             PointF((width / 4).toFloat(), (-height / 5.3).toFloat()),
             (width / 4).toFloat(), 60
         )
-        val circleBean3 = CircleBean(
+        val cb3 = CircleBean(
             PointF((-width / 12).toFloat(), (height / 1.1).toFloat()),
             PointF((centerX - 100).toFloat(), (height * 2 / 3).toFloat()),
             PointF((width / 3.4).toFloat(), (height / 2).toFloat()),
@@ -92,7 +91,7 @@ class SplashFragment :
             (width / 24).toFloat(), 60
         )
 
-        val circleBean4 = CircleBean(
+        val cb4 = CircleBean(
             PointF((-width / 9).toFloat(), (height / 0.9).toFloat()),
             PointF(centerX.toFloat(), (height * 3 / 4).toFloat()),
             PointF((width / 2.1).toFloat(), (height / 2.3).toFloat()),
@@ -101,7 +100,7 @@ class SplashFragment :
             (width / 4).toFloat(), 60
         )
 
-        val circleBean5 = CircleBean(
+        val cb5 = CircleBean(
             PointF((width / 1.4).toFloat(), (height / 0.9).toFloat()),
             PointF(centerX.toFloat(), (height * 3 / 4).toFloat()),
             PointF((width / 2).toFloat(), (height / 2.37).toFloat()),
@@ -109,7 +108,7 @@ class SplashFragment :
             PointF((width / 2).toFloat(), (-height / 7.1).toFloat()),
             (width / 4).toFloat(), 60
         )
-        val circleBean6 = CircleBean(
+        val cb6 = CircleBean(
             PointF((width / 0.8).toFloat(), height.toFloat()),
             PointF((centerX + 20).toFloat(), (height * 2 / 3).toFloat()),
             PointF((width / 1.9).toFloat(), (height / 2.3).toFloat()),
@@ -117,7 +116,7 @@ class SplashFragment :
             PointF((width / 1.1).toFloat(), (-height / 6.4).toFloat()),
             (width / 4).toFloat(), 60
         )
-        val circleBean7 = CircleBean(
+        val cb7 = CircleBean(
             PointF((width / 0.9).toFloat(), (height / 1.2).toFloat()),
             PointF((centerX + 20).toFloat(), (height * 4 / 7).toFloat()),
             PointF((width / 1.6).toFloat(), (height / 1.9).toFloat()),
@@ -125,17 +124,7 @@ class SplashFragment :
             PointF(width.toFloat(), 0F),
             (width / 9.6).toFloat(), 60
         )
-
-        circleBeanList.apply {
-            add(circleBean)
-            add(circleBean2)
-            add(circleBean3)
-            add(circleBean4)
-            add(circleBean5)
-            add(circleBean6)
-            add(circleBean7)
-        }
-
+        circleBeanList.addAll(0, listOf(cb1, cb2, cb3, cb4, cb5, cb6, cb7))
     }
 
 
